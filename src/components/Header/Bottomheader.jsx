6d4067d65913a1b56813c7b838fd9869234f13e2
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-05-31 21:12:43 +0300

    add links

--- src/components/Header/Bottomheader.jsx
+++ src/components/Header/Bottomheader.jsx
@@ -1,11 +1,12 @@
+import{Link} from "react-router-dom"
 function Bottomheader(){
     return(
         <nav className="navigation_bar">
             <ol  className="nav_list">
-                <li className="nav_item"><a href="HOME">HOME</a></li>
-                <li className="nav_item"><a href="HOME">OUR LEADERS</a></li>
-                <li className="nav_item"><a href="HOME">AVAILABLE TRACKS</a></li>
-                <li className="nav_item"><a href="HOME">EVENTS</a></li>
+                <li className="nav_item"><Link to="/">HOME</Link></li>
+                <li className="nav_item"><Link to="/Leaders">OUR LEADERS</Link></li>
+                <li className="nav_item"><Link to="/Tracks">AVAILABLE TRACKS</Link></li>
+                <li className="nav_item"><Link to="/Events">EVENTS</Link></li>
 
             </ol>
         </nav>
