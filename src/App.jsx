0917a022bb89bb8fcfc6cdb866880587fd6f51e5
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-05-31 16:27:35 +0300

    configure css

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
 
-import './App.css'
-
-function Student({fname,lname}){
-return(
-  <div>
-    <h1>{fname} is his first name</h1>
-    {lname && <a href={lname}>{lname} is his facebook name</a>}
-  </div>
-)
-}
-
 function App() {
 
   return (
     <>
 <h1>Hello world!</h1>
-<Student fname={'joseph'} lname={"mbugua"}/>
 
 
     </>
