820ed787e35875454cee249c6d9f319ed9dd54cc
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-05-31 21:26:20 +0300

    add a style to bottom navigation bar

--- src/components/Header/Bottomheader.jsx
+++ src/components/Header/Bottomheader.jsx
@@ -1,8 +1,14 @@
 function Bottomheader(){
     return(
-        <div>
-            <p>bottom header</p>
-        </div>
+        <nav className="navigation_bar">
+            <ol  className="nav_list">
+                <li className="nav_item"><a href="HOME">HOME</a></li>
+                <li className="nav_item"><a href="HOME">OUR LEADERS</a></li>
+                <li className="nav_item"><a href="HOME">AVAILABLE TRACKS</a></li>
+                <li className="nav_item"><a href="HOME">EVENTS</a></li>
+
+            </ol>
+        </nav>
     )
 }
 export default Bottomheader;
