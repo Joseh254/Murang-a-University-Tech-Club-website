8bae64a205a881b2a2392e73f3371b84e91f517b
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-05-31 21:21:40 +0300

    add about section

--- src/Pages/Home/About.jsx
+++ src/Pages/Home/About.jsx
@@ -13,12 +13,16 @@ function About(){
     return(
 
      <div className="about">
+        <div className="socials">
+            <p>Our social platforms</p>
         <Social icon={<FaInstagramSquare/>} description={"Instagram"}/>
         <Social icon={<FaFacebookF/>} description={"facebook"}/>
         <Social icon={<FaXTwitter/>} description={"Twitter"}/>
+        </div>
 
         <div className="vision">
-            <h1>Our vision</h1>
+            <h1>About us</h1>
+            <h2>Our vision</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum harum molestias rerum, numquam, consectetur in, reiciendis dolorem inventore itaque temporibus sed ipsa! Molestias est obcaecati officia deserunt odit non enim.</p>
         </div>
      </div>
---
+++ src/components/Header/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer(){
+return(
+
+    <div>
+        footer
+    </div>
+)
+}
