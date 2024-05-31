6607afa574b3230898b5adb30454a72559f13a86
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-05-31 12:04:38 +0300

    print hello world to see if it is working as expected

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Me from "./Me"
+
+function Myfirstcomponent(){
+  return(
+    <div>
+      <h1>This is a component</h1>
+    </div>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+<h1>Hello world!</h1>
+<Myfirstcomponent/>
+<Myfirstcomponent/>
+<Myfirstcomponent/>
+<Me/>
     </>
   )
 }
---
+++ src/Me.jsx
@@ -0,0 +1,7 @@
+function Me(){
+    return(
+        <p>Hello Joseph!</p>
+    )
+}
+
+export default Me;
