c14b8db0f7c80731c82326442a8036ec0fb496c1
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-06-01 15:26:40 +0300

    add hero section to tracks page

--- src/Pages/Tracks/Tracks.jsx
+++ src/Pages/Tracks/Tracks.jsx
@@ -1,5 +1,17 @@
 import "./Tracks.css";
 function Tracks() {
-  return <div></div>;
+  return <div>
+
+    <section className="tracks">
+      <div className="introduct">
+        <h1>This is a heading in track section</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error natus veniam impedit officiis architecto eveniet a quaerat deserunt repellat dicta! Aliquid repudiandae consectetur, temporibus quam corporis excepturi. Facere, eaque dolorem?</p>
+      </div>
+
+      
+
+
+    </section>
+  </div>;
 }
 export default Tracks;
