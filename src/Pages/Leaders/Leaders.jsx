ffeebe9eecd93b108dfaac9908d1ddf94a8691b5
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-06-01 15:33:55 +0300

    -

--- src/Pages/Leaders/Leaders.jsx
+++ src/Pages/Leaders/Leaders.jsx
@@ -8,6 +8,124 @@ function Leaders() {
         </h1>
         <p>Dedicated Leaders Driving Innovation and Excellence </p>
       </div>
+{/* **************************************************************************************88 */}
+      <div className="ourleaders">
+
+       
+       <div className="ndia">
+        <img src="" alt="dr John ndia" />
+        <h1>Dr. John Ndia</h1>
+        <p>
+        Dr. John Ndia is a distinguished professor and an esteemed patron of the Murang'a University Tech Club. With over 20 years of experience in the field of computer science and technology, 
+        </p>
+      </div>
+{/* **************************************************************************************************8 */}
+      <div className="victory">
+        <img src="" alt="Victory Njeri" />
+        <h1>Victory Njeri</h1>
+        <p>
+        Dr. John Ndia is a distinguished professor and an esteemed patron of the Murang'a University Tech Club. With over 20 years of experience in the field of computer science and technology,
+        </p>
+      </div>
+      {/* ****************************************************************************************************8 */}
+      <div className="allan">
+        <img src="" alt="allan" />
+        <h1>Allan Muhari</h1>
+        <p>
+        Dr. John Ndia is a distinguished professor and an esteemed patron of the Murang'a University Tech Club. With over 20 years of experience in the field of computer science and technology, 
+        </p>
+      </div>
+      {/* ************************************************************************************************* */}
+      <div className="bridget">
+        <img src="" alt="bridget" />
+        <h1>Bridget Gitonga</h1>
+        <p>
+        Dr. John Ndia is a distinguished professor and an esteemed patron of the Murang'a University Tech Club. With over 20 years of experience in the field of computer science and technology, b.
+        </p>
+      </div>
+{/* ******************************************************************************************************* */}
+</div>
+
+<div className="ourleaders">
+<div className="ruth">
+        <img src="" alt="Ruth Mutisya" />
+        <h1>Ruth Mutisya</h1>
+        <p>
+        Dr. John Ndia is a distinguished professor and an esteemed patron of the Murang'a University Tech Club. With over 20 years of experience in the field of computer science and technology,.
+        </p>
+      </div>
+{/* ********************************************************************************************* */}
+       
+<div className="webster">
+        <img src="" alt="Webster Ifedha" />
+        <h1>Webster Ifedha</h1>
+        <p>
+        Dr. John Ndia is a distinguished professor and an esteemed patron of the Murang'a University Tech Club. With over 20 years of experience in the field of computer science and technology,.
+        </p>
+      </div>
+
+      {/* ********************************************************************************************8 */}
+       
+
+      <div className="manase">
+        <img src="" alt="Manase Gunga" />
+        <h1>Manase Gunga</h1>
+        <p>
+        Dr. John Ndia is a distinguished professor and an esteemed patron of the Murang'a University Tech Club. With over 20 years of experience in the field of computer science and technology, 
+        </p>
+      </div>
+
+      {/* ******************************************************************************************** */}
+       
+       {/* ********************************************************************************************* */}
+       
+<div className="caro">
+        <img src="" alt="Carolyne Githenduka" />
+        <h1>Carolyne Githenduka</h1>
+        <p>
+        Dr. John Ndia is a distinguished professor and an esteemed patron of the Murang'a University Tech Club. With over 20 years of experience in the field of computer science and technology, Dr. Ndia has been instrumental in guiding the club's activities and initiatives..
+        </p>
+      </div>
+
+      {/* ********************************************************************************************8 */}
+      </div>
+<div className="ourleaders">
+      <div className="stanley">
+        <img src="" alt="Stanley Amunze" />
+        <h1>Stanley Amunze</h1>
+        <p>
+        Dr. John Ndia is a distinguished professor and an esteemed patron of the Murang'a University Tech Club. With over 20 years of experience in the field of computer science and technology, Dr. Ndia has been instrumental in guiding the club's activities and initiatives.
+        </p>
+      </div>
+
+      {/* ******************************************************************************************** */}
+       
+
+      <div className="caro">
+        <img src="" alt="Carolyne Githenduka" />
+        <h1>Carolyne Githenduka</h1>
+        <p>
+        Dr. John Ndia is a distinguished professor and an esteemed patron of the Murang'a University Tech Club. With over 20 years of experience in the field of computer science and technology, Dr. Ndia has been instrumental in guiding the club's activities and initiatives..
+        </p>
+      </div>
+
+      {/* ********************************************************************************************8 */}
+      </div>
+
+
+      <div className="caro">
+        <img src="" alt="Carolyne Githenduka" />
+        <h1>Carolyne Githenduka</h1>
+        <p>
+        Dr. John Ndia is a distinguished professor and an esteemed patron of the Murang'a University Tech Club. With over 20 years of experience in the field of computer science and technology, Dr. Ndia has been instrumental in guiding the club's activities and initiatives..
+        </p>
+      </div>
+
+      {/* ********************************************************************************************8 */}
+
+      
+
+
     </section>
   );
 }
