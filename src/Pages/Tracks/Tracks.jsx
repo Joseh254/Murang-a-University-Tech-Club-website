c04e574cf8204de0d11f9c4837c9a4a7e9b9fef4
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-06-01 15:31:31 +0300

    add content of available tracks

--- src/Pages/Tracks/Tracks.jsx
+++ src/Pages/Tracks/Tracks.jsx
@@ -9,9 +9,22 @@ function Tracks() {
       </div>
 
       {/* ************************************************************************************************ */}
-  <div>
+  <div className="part one">
+
+  <div className="them_tracks">
     <h2>Cyber security</h2>
     <p>Students who choose the Cyber Security track will delve into the critical aspects of protecting systems, networks, and data from cyber threats. Throughout this track, they will learn about various security protocols, ethical hacking, encryption techniques, and threat analysis. They will engage in hands-on projects and real-world scenarios to apply their knowledge in identifying and mitigating security vulnerabilities. By the end of the track, students will be equipped with the skills to safeguard digital environments, conduct security audits, and implement robust security measures to protect against cyber attacks.</p>
+  </div>
+
+
+  <div className="them_tracks">
+    <h2>UI/UX design</h2>
+    <p>Students who choose the Cyber Security track will delve into the critical aspects of protecting systems, networks, and data from cyber threats. Throughout this track, they will learn about various security protocols, ethical hacking, encryption techniques, and threat analysis. They will engage in hands-on projects and real-world scenarios to apply their knowledge in identifying and mitigating security vulnerabilities. By the end of the track, students will be equipped with the skills to safeguard digital environments, conduct security audits, and implement robust security measures to protect against cyber attacks.</p>
+  </div>
+
+  
+
+
   </div>
 
     </section>
