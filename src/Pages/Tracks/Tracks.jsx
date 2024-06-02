d1532a9e86d4e95a14d7a3b257be4e845da1251d
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-06-02 22:10:36 +0300

    replace the origina content

--- src/Pages/Tracks/Tracks.jsx
+++ src/Pages/Tracks/Tracks.jsx
@@ -35,20 +35,20 @@ function Tracks() {
 
   <div className="them_tracks">
     <h2>Mobile apps development  </h2>
-    <p>Students who choose the Cyber Security track will delve into the critical aspects of protecting systems, networks, and data from cyber threats. Throughout this track, they will learn about various security protocols, ethical hacking, encryption techniques, and threat analysis. They will engage in hands-on projects and real-world scenarios to apply their knowledge in identifying and mitigating security vulnerabilities. By the end of the track, students will be equipped with the skills to safeguard digital environments, conduct security audits, and implement robust security measures to protect against cyber attacks.</p>
+    <p> Opt for the Mobile Apps Development track and learn to design and build applications for mobile devices. You will explore the basics of mobile app development using platforms such as Android and iOS, gaining proficiency in languages like Java, Kotlin, Swift, and Dart. Through hands-on projects and interactive sessions, you will develop the skills to create user-friendly and efficient mobile applications. By the end of the track, you will be adept at building, testing, and deploying mobile apps, ready to deliver engaging and high-performance solutions for smartphones and tablets.</p>
   </div>
 
 
   <div className="them_tracks">
     <h2>Cloud engineering </h2>
-    <p>Students who choose the Cyber Security track will delve into the critical aspects of protecting systems, networks, and data from cyber threats. Throughout this track, they will learn about various security protocols, ethical hacking, encryption techniques, and threat analysis. They will engage in hands-on projects and real-world scenarios to apply their knowledge in identifying and mitigating security vulnerabilities. By the end of the track, students will be equipped with the skills to safeguard digital environments, conduct security audits, and implement robust security measures to protect against cyber attacks.</p>
+    <p>Choose the Cloud Engineering track and dive into the world of cloud computing and its applications. Youwill learn about various cloud service providers such as AWS, Google Cloud, and Microsoft Azure, and understand how to design, deploy, and manage scalable and secure cloud infrastructures. Through practical projects and hands-on labs,you will gain experience in cloud architecture, virtualization, containerization, and DevOps practices. By the end of the track,you will be capable of leveraging cloud technologies to build and maintain robust cloud-based solutions, ensuring high availability and performance for applications and services.</p>
   </div>
 
 
 
   <div className="them_tracks">
     <h2>Power Platform </h2>
-    <p>Students who choose the Cyber Security track will delve into the critical aspects of protecting systems, networks, and data from cyber threats. Throughout this track, they will learn about various security protocols, ethical hacking, encryption techniques, and threat analysis. They will engage in hands-on projects and real-world scenarios to apply their knowledge in identifying and mitigating security vulnerabilities. By the end of the track, students will be equipped with the skills to safeguard digital environments, conduct security audits, and implement robust security measures to protect against cyber attacks.</p>
+    <p>Students who select the Power Platform track will explore the capabilities of Microsoft's Power Platform, which includes Power BI, Power Apps, Power Automate, and Power Virtual Agents. They will learn how to create custom business applications, automate workflows, analyze data, and build chatbots with minimal coding. Through hands-on projects and interactive workshops, students will gain practical experience in leveraging these tools to enhance business processes and drive data-driven decision-making. By the end of the track, students will be proficient in developing integrated solutions using the Power Platform, enabling them to transform organizational operations and improve efficiency.</p>
   </div>
 
   </div>
