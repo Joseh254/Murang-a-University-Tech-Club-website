180470a050157273a6a5af2d427211e27332b874
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-06-02 22:11:52 +0300

    -

--- src/Pages/Tracks/Tracks.jsx
+++ src/Pages/Tracks/Tracks.jsx
@@ -48,7 +48,7 @@ function Tracks() {
 
   <div className="them_tracks">
     <h2>Power Platform </h2>
-    <p>Students who select the Power Platform track will explore the capabilities of Microsoft's Power Platform, which includes Power BI, Power Apps, Power Automate, and Power Virtual Agents. They will learn how to create custom business applications, automate workflows, analyze data, and build chatbots with minimal coding. Through hands-on projects and interactive workshops, students will gain practical experience in leveraging these tools to enhance business processes and drive data-driven decision-making. By the end of the track, students will be proficient in developing integrated solutions using the Power Platform, enabling them to transform organizational operations and improve efficiency.</p>
+    <p>Sselect the Power Platform track and explore the capabilities of Microsoft's Power Platform, which includes Power BI, Power Apps, Power Automate, and Power Virtual Agents. you will learn how to create custom business applications, automate workflows, analyze data, and build chatbots with minimal coding. Through hands-on projects and interactive workshops, you will gain practical experience in leveraging these tools to enhance business processes and drive data-driven decision-making. By the end of the track, you will be proficient in developing integrated solutions using the Power Platform, enabling them to transform organizational operations and improve efficiency.</p>
   </div>
 
   </div>
